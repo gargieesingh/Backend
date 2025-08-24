18087ce6a3c255f9be862060393e3bfc591d9641
Gargie <gargiesingh321@gmail.com>
2025-08-24 15:48:35 +0530

    Added axios

--- Full-Stack-Basic/Frontend/src/App.jsx
+++ Full-Stack-Basic/Frontend/src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [jokes, setJokes] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/jokes')
+    .then((response)=>{
+      setJokes(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  })
   return (
     <>
       <h1>Full Stack</h1>
       <p>JOKES: {jokes.length} </p>
 
       {
-        jokes.map((joke, index) =>{
+        jokes.map((joke) =>{
           <div key = {joke.id}>
             <h3>{joke.title}</h3>
             <p>{joke.content}</p>
