fce94fc449bce330dcd12ec18c8a562879dd48db
Gargie <gargiesingh321@gmail.com>
2025-09-02 20:43:36 +0530

    Learned about npm , dependencies, devdependencies, modules

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,7 +1,39 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const { findPackageJSON } = require('module');
 
-//fs.writeFile(file, data[, options], callback)
-fs.writeFile("hey.txt", "hey hello aise ho", function(err){
-    if(err) console.error(err);
-    else console.log("done");
-})
+// //fs.writeFile(file, data[, options], callback)
+// fs.appendFile("hey.txt", " mai toh achha hu", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.rename("hey.txt", "hello.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// // fs.copyFile(src, dest[, mode], callback)
+// fs.copyFile("hello.txt", "./copy2/chacha.txt", function(err){
+//     if(err) console.error(err.message);
+//     else console.log("done");
+// })
+
+// fs.unlink("hello.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("removed");
+// })
+
+// fs.rm("./copy",{recursive: true}, function(err){
+//     if(err) console.error(err.message);
+//     else console.log("Removed");
+// })
+
+// const http = require('http');
+// const server = http.createServer(function(req, res){
+//     res.end("hello world");
+// })
+
+// server.listen(3000);
+//
+
+
