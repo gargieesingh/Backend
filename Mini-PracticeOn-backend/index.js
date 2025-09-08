06db0ffc016fb38c23652930822b5d0f13122d0a
Gargie <gargiesingh321@gmail.com>
2025-09-08 18:07:09 +0530

    Installed ejs

--- Mini-PracticeOn-backend/index.js
+++ Mini-PracticeOn-backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+//parsers
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs'); //backend will render(view) --> ejs pages
 
 //route setup
 app.get("/", function(req, res){
