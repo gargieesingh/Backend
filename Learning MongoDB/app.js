29e8ee291ae24ea9567c4c38c0adbf809b0ee65b
Gargie <gargiesingh321@gmail.com>
2025-10-08 00:04:36 +0530

    feat: Add update , delete , read feature using mongoose

--- Learning MongoDB/app.js
+++ Learning MongoDB/app.js
@@ -6,13 +6,30 @@ app.get('/', (req, res)=>{
     res.send("hey");
 })
 
-app.get('/create', (req, res)=>{
-    userModel.create({
-        name: "Gargie",
-        email: "gargie@gmail.com",
-        username: "gracy"
+app.get('/create', async (req, res)=>{
+    let createduser = await userModel.create({
+        name: "Gargie hf",
+        email: "gargiehf@gmail.com",
+        username: "gracyy"
     })
+    res.send(createduser)
 })
 
+app.get('/update', async (req, res)=>{
+    let updateduser = await userModel.findOneAndUpdate({username: "gracy"}, {name: "Gargie Singh"}, {new: true})
+    res.send(updateduser)
+})
+
+app.get("/read",  async function(req, res){
+    let users = await userModel.find({username: "gracy"});
+    res.send(users);
+})
+
+app.get("/delete", async(req, res)=>{
+    let users = await userModel.findOneAndDelete({username: "gracy"});
+    res.send(users);
+})
 //upper code is asynchronous, hence , niche wala code will run first, then upper wala, (info by mongoose)
+
+//mongoose se related koi bhi code asynchronous hoga, we can use async await in it.
 app.listen(3000);
