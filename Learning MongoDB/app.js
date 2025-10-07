cccb91b1f2ffe22d6c82f7ea179ebf0fa7d0488b
Gargie <gargiesingh321@gmail.com>
2025-10-07 23:42:41 +0530

    feat: add create user feature

--- Learning MongoDB/app.js
+++ Learning MongoDB/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const userModel = require('./usermodel');
 
 app.get('/', (req, res)=>{
     res.send("hey");
 })
+
+app.get('/create', (req, res)=>{
+    userModel.create({
+        name: "Gargie",
+        email: "gargie@gmail.com",
+        username: "gracy"
+    })
+})
+
+//upper code is asynchronous, hence , niche wala code will run first, then upper wala, (info by mongoose)
 app.listen(3000);
--- Learning MongoDB/usermodel.js
+++ Learning MongoDB/usermodel.js
@@ -10,4 +10,6 @@ const userSchema = mongoose.Schema({ //method that accepts an object
 //creating model(for crud)
 mongoose.model("user", userSchema);
 
+//we can also apply crud operations here, but we want to do that in some route, e.g. /delete for deleting , /register for creating etc.
 
+module.exports = mongoose.model("user", userSchema);
