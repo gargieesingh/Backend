6fd94dda492bcb1888f3d0b7d4be777124abb0c7
Gargie <gargiesingh321@gmail.com>
2025-10-12 00:05:25 +0530

    feat(update): Add edit user feature

--- Learning MongoDB/CRUD with ejs/app.js
+++ Learning MongoDB/CRUD with ejs/app.js
@@ -22,6 +22,17 @@ app.get('/delete/:id', async (req,res)=>{
     res.redirect("/read");
 })
 
+app.get('/edit/:id', async(req, res)=>{
+    let user = await userModel.findOne({_id: req.params.id});
+    res.render('edit', {user});
+})
+
+app.post('/update/:id', async(req, res)=>{
+    let{name, email, image} = req.body;
+    let user = await userModel.findOneAndUpdate({_id: req.params.id}, {name, email, image}, {new: true});
+    res.redirect('/read');
+})
+
 app.post('/create', async (req,res)=>{
     let{name, email, image} = req.body;
     let Createduser = await userModel.create({
