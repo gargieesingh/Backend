8055681a50385387391128bbdb894b83236ac541
Gargie <gargiesingh321@gmail.com>
2025-10-11 20:19:26 +0530

    feat(models): implement user model and establish MongoDB connnection

---
+++ Learning MongoDB/CRUD with ejs/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://127.0.0.1:27017/testapp")
+    .then(()=> console.log("MongoDB connected"))
+    .catch(err => console.log("Connection error: ", err));
+
+const userSchema = mongoose.Schema({
+    image: String,
+    name: String,
+    email: String
+})
+
+module.exports = mongoose.model('user', userSchema);
