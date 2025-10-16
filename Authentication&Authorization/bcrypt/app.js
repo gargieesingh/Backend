af1c8ec773c3671a6f520ae1fcb35c6738eeecd5
Gargie <gargiesingh321@gmail.com>
2025-10-16 23:16:24 +0530

    learned bcrypt

--- Authentication&Authorization/bcrypt/app.js
+++ Authentication&Authorization/bcrypt/app.js
@@ -1,1 +1,29 @@
 const bcrypt = require("bcryptjs");
+const express = require("express");
+const app = express();
+
+const password = "mySecret123";
+
+const saltRounds = 10;
+
+//hashing the password
+bcrypt.hash(password, saltRounds, (err, hash)=>{
+    if(err){
+        console.error("Error hashing password: ", err)
+        return;
+    }
+    
+    console.log("Hashed password: ", hash);
+    
+    bcrypt.compare(password, hash, (err, result)=>{
+        if(err){
+            console.error("Error comparing passwords: ", err);
+            return;
+        }
+        if(result){
+            console.log("Passwords match!");
+        }else{
+            console.log("Passwords do not match!");
+        }
+    })
+})
